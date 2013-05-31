24424a9a57bd5704e0a7d314102d33641a404c57
Ben Bleything <ben@bleything.net>
2013-05-31 13:58:01 -0700

    run apt-get update before installing stuff

--- cookbooks/graphite/recipes/default.rb
+++ cookbooks/graphite/recipes/default.rb
@@ -7,6 +7,8 @@
 # Released under the terms of the MIT License. See README.md for details.
 #
 
+execute "apt-get update"
+
 include_recipe "graphite::whisper"
 include_recipe "graphite::carbon"
 include_recipe "graphite::web"
